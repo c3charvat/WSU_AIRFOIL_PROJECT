38a6d0a813395905959ea350ffa190891145ecce
c3charvat <c3charvat@gmail.com>
2021-11-19 23:18:33 -0500

    Spelling

--- Firmware_V11/src/Driver_Setup.ino
+++ Firmware_V11/src/Driver_Setup.ino
@@ -52,7 +52,7 @@ driverE2.microsteps(64);
 driverE2.pwm_ofs_auto ();
 
 driverE3.beginSerial(115200);
-Serial.println("Driver X Enabled\n");
+Serial.println("Driver E3 Enabled\n");
 driverE1.begin();
 driverE1.rms_current(850); // mA
 driverE1.microsteps(64);
