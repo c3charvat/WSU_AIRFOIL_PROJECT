bba4d42b87d6e1c07b755ced69e0e2162ebbca72
c3charvat <c3charvat@gmail.com>
2022-11-06 23:09:59 -0500

    Movement_struct_init

--- Firmware_V12/src/Movement.cpp
+++ Firmware_V12/src/Movement.cpp
@@ -1,3 +1,12 @@
 #include "Movement.hpp"
+#include "Data_structures.h"
 
-//void initialize_Movement_Structs( *name ){
+void initialize_Movement_Struct(struct PositionStruct *pos, struct ControlStruct *control){
+    pos->xpos=0;
+    pos->ypos=0;
+    pos->aoatpos=0;
+    pos->aoabpos=0;
+    control->wificonnected = false;
+    control->usbconnected =false;
+    pos->source=control; // goal here is to create a link to a single structre that will update with what interface has control
+}
--- Firmware_V12/src/Movement.hpp
+++ Firmware_V12/src/Movement.hpp
@@ -1,9 +1,8 @@
-#ifndef Movements_Hpp
-#define Movements_Hpp
+#ifndef Movement_Hpp
+#define Movement_Hpp
 
 
-
-//void initialize_Movement_Structs(*name);
+void initialize_Movement_Struct(struct PositionStruct *pos, struct ControlStruct *control);
 
 
 #endif
