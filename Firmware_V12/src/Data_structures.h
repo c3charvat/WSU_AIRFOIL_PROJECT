e7f0a58f698a13bdfe6e4e284fad4a2e57587991
collin.charvat <collin.charvat@keithley.com>
2022-11-07 13:49:59 -0500

    Add_Error_Struct

--- Firmware_V12/src/Data_structures.h
+++ Firmware_V12/src/Data_structures.h
@@ -2,7 +2,8 @@
 #ifndef DATASTRUCT_H
 #define DATASTRUCT_H
 
-// structure definitions 
+///// structure definitions 
+/// Packet structs (To be sent)
 struct __attribute__((packed)) ConnectTestStruct{
   char name;
   bool connected;
@@ -25,6 +26,15 @@ struct __attribute__((packed)) PositionStruct {
 
 //typedef struct PositionStruct positionstruct; // if i decide to make it a tyoe later
 
+/// Internal Structs
 
+struct Error {
+  String error_name;
+  String error_information_1;
+  String error_information_2;
+  String error_information_3;
+  String originating_function_name;
+  bool recoverable;
+}
 
 #endif
