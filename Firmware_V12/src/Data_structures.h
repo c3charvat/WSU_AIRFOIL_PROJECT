a8633398cd860d27f571227a9e9df87d705f6f41
c3charvat <c3charvat@gmail.com>
2023-10-31 20:15:31 -0400

    feat:pointer_help

--- Firmware_V12/src/Data_structures.h
+++ Firmware_V12/src/Data_structures.h
@@ -43,3 +43,59 @@ struct DriverSettings{
   int aoabdirver_microstepping;
 };
 #endif
+
+/*
+Pointer refrence:
+
+string* hello(string* charBuffer){
+	cout<< charBuffer << "\n"; //address
+    cout<< *charBuffer << "\n";//value Pizza
+    return charBuffer;// address 
+}
+
+int main() {
+  string food = "Pizza";
+  string* ptr = &food;
+  
+  cout << hello(&food);
+
+
+// Output the value of food (Pizza)
+cout << food << "\n";
+
+// Output the memory address of food (0x6dfed4)
+cout << &food << "\n";
+
+// Access the memory address of food and output its value (Pizza)
+cout << *ptr << "\n";
+
+// Change the value of the pointer
+*ptr = "Hamburger";
+
+// Output the new value of the pointer (Hamburger)
+cout << *ptr << "\n";
+
+// Output the new value of the food variable (Hamburger)
+cout << food << "\n";
+
+// create the data strucures
+  // ConnectStatusStruct *Connectionstatus_ptr, Connectionstatus; // initalise a pointer to a strcut of connect test and
+  // Connectionstatus_ptr = &Connectionstatus;
+  // ControlStruct *Source_ptr, Source;
+  // Source_ptr = &Source;
+  // PositionStruct *RecievedData_ptr, RecievedData;
+  // RecievedData_ptr = &RecievedData;
+  // PositionStruct *CurrentPostions_ptr, CurrentPostions;
+  // CurrentPostions_ptr = &CurrentPostions;
+
+  // // Error Struct
+
+  // Error *Error_ptr, Error_struct;
+  // Error_ptr = &Error_struct;
+
+  // // initilize the structures
+  // initialize_movement_struct(CurrentPostions_ptr, Source_ptr);
+  // initialize_movement_struct(RecievedData_ptr, Source_ptr);
+
+
+*/
